c2056b8414aca1ef743a1c5a481d2fc7ec05aa02
Giuliano Faustini <giuliperus@gmail.com>
2023-10-30 11:32:22 +0200

    Added addnote and update importace with mutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import React from "react"
-import { useQuery } from "@tanstack/react-query"
-import { getNotes } from "./requests"
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { getNotes, createNote, updateNote } from "./requests"
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    },
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
+    newNoteMutation.mutate({content, important: true})
     console.log(content)
   }
+
+
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    },
+  })
+
   const toggleImportance = (note) => {
+    updateNoteMutation.mutate({...note, important: !note.important})
     console.log('toggle importance of', note.id)
   }
 
--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
-
+const baseUrl = "http://localhost:3001/notes";
 
 // move the function making the actual HTTP request to its own file requests.js
-export const getNotes = () =>
-  axios.get('http://localhost:3001/notes').then(res => res.data)
+export const getNotes = () => axios.get(baseUrl).then((res) => res.data);
+
+export const createNote = (newNote) =>
+  axios.post(baseUrl, newNote).then((res) => res.data);
+
+export const updateNote = (updatedNote) =>
+  axios
+    .put(`${baseUrl}/${updatedNote.id}`, updatedNote)
+    .then((res) => res.data);
